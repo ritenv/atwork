8560a37520e94bff81a37c97efbbeda8d0332701
Riten Vagadiya <riten.s.v@gmail.com>
2015-02-10 14:19:27 +0400

    Cleanup

--- tools/test/mocha-req.js
+++ tools/test/mocha-req.js
@@ -2,5 +2,4 @@
 
 process.env.NODE_ENV = 'development';
 var appRoot = __dirname + '/../../';
-require(appRoot + 'index.js');
-// require('meanio/lib/util').preload(appRoot + '/packages/**/server', 'model');
+require(appRoot + 'index');
