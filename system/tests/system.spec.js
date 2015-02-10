9376b6ff59d7f91cb0072badaec0cb04102a02f7
Riten Vagadiya <riten.s.v@gmail.com>
2015-02-10 14:11:23 +0400

    Simple spec to test the system module

--- system/tests/system.spec.js
+++ system/tests/system.spec.js
@@ -6,31 +6,13 @@
   // Articles Controller Spec
   describe('System', function() {
     describe('SystemServices', function() {
-      // The $resource service augments the response object with methods for updating and deleting the resource.
-      // If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
-      // the responses exactly. To solve the problem, we use a newly-defined toEqualData Jasmine matcher.
-      // When the toEqualData matcher compares two objects, it takes only object properties into
-      // account and ignores methods.
-      // beforeEach(function() {
-      //   jasmine.addMatchers({
-      //     toEqualData: function() {
-      //       return {
-      //         compare: function(actual, expected) {
-      //           return {
-      //             pass: angular.equals(actual, expected)
-      //           };
-      //         }
-      //       };
-      //     }
-      //   });
-      // });
-
       beforeEach(function() {
         module('atwork.system');
       });
 
       // Initialize the controller and a mock scope
       var appSearch,
+          appStorage,
           tokenHttpInterceptor,
           scope,
           $httpBackend,
@@ -40,11 +22,13 @@
       // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
       // This allows us to inject a service but then attach it to a variable
       // with the same name as the service.
-      beforeEach(inject(function($rootScope, _$location_, _$httpBackend_, _appSearch_, _tokenHttpInterceptor_) {
+      beforeEach(inject(function($rootScope, _$location_, _$httpBackend_, _appSearch_, _appStorage_, _tokenHttpInterceptor_) {
 
         scope = $rootScope.$new();
 
         appSearch = _appSearch_;
+        appStorage = _appStorage_;
+        tokenHttpInterceptor = _tokenHttpInterceptor_;
 
         $httpBackend = _$httpBackend_;
 
@@ -52,10 +36,19 @@
 
       }));
 
-      it('$scope.find() should create an array with at least one article object ' +
-        'fetched from XHR', function() {
-          expect(true).to.equal(true);
-        });
+      it('Service appSearch should be available', function() {
+          expect(appSearch).to.be.a('function');
+      });
+      it('Service tokenHttpInterceptor should be available', function() {
+          expect(tokenHttpInterceptor).to.be.a('object');
+          
+          var config = {headers: {}};
+          appStorage.set('userToken', 'sample token');
+          config = tokenHttpInterceptor.request(config);
+          expect(config.headers.Authorization).to.be.a('string');
+          expect(config.headers.Authorization).to.be.equal('Bearer sample token');
+      });
+
     });
   });
 }());
