7fdcb0027d9fc99725fc5adb79cf60ebf29d5bbb
Riten Vagadiya <riten.s.v@gmail.com>
2015-03-14 17:48:32 +0400

    Production config for heroku

---
+++ system/config/production.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  db: 'mongodb://ritenv:123.com@ds055680.mongolab.com:55680/atwork-dev',
+  server: {
+    host: 'localhost',
+    port: 8111
+  },
+  secret: 'atworksecret'
+};
