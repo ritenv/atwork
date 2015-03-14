8340aeff56d67abe2c3391a31b2792d11292ff8d
Riten Vagadiya <riten.s.v@gmail.com>
2015-03-14 17:56:19 +0400

    Changes for heroku

--- system/config/production.js
+++ system/config/production.js
@@ -4,7 +4,7 @@ module.exports = {
   db: 'mongodb://ritenv:123.com@ds055680.mongolab.com:55680/atwork-dev',
   server: {
     host: 'localhost',
-    port: 8111
+    port: process.env.PORT || 8111
   },
   secret: 'atworksecret'
 };
