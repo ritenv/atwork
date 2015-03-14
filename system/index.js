5e5a7b41d397a8df6dda3c9454415e2b3dd25b7c
Riten Vagadiya <riten.s.v@gmail.com>
2015-03-14 18:27:44 +0400

    Production by default

--- system/index.js
+++ system/index.js
@@ -7,7 +7,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var mongoose = require('mongoose');
-var Config = require('./config/' + (process.env.npm_config_production?'production' : 'development'));
+var Config = require('./config/' + (process.env.npm_config_dev ? 'development' : 'production'));
 console.log(process.env);
 console.log(Config);
 var bodyParser = require('body-parser');
