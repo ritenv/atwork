965849d745b2c573e4d3468f7929e862ab598e08
Riten Vagadiya <riten.s.v@gmail.com>
2015-05-01 13:41:11 +0400

    Changed resolved values to come from function, added root to show feeds as well

--- modules/posts/public/posts.routes.js
+++ modules/posts/public/posts.routes.js
@@ -7,54 +7,45 @@ angular.module('atwork.posts')
         templateUrl: '/modules/posts/views/feed.html',
         controller: 'PostsCtrl',
         resolve: {
-          resolvedFeeds: [
-            '$route',
-            'appPostsFeed',
-            function($route, appPostsFeed) {
-              var deferred = Q.defer();
-              var options = angular.extend({
-                // feedsFilter: $scope.feedsFilter,
-                limitComments: true,
-                feedPage: 0
-              }, $route.current.params);
-
-              appPostsFeed.getFeeds(options, function(response) {
-                deferred.resolve(response);
-              });
-
-              return deferred.promise;
-            }
-          ]
+         resolvedFeeds: resolvedFeeds({limitComments: true})
         }
       })
       .when('/feed/:hashtag', {
         templateUrl: '/modules/posts/views/feed.html'
       })
       .when('/', {
-        templateUrl: '/modules/posts/views/feed.html'
+        templateUrl: '/modules/posts/views/feed.html',
+        controller: 'PostsCtrl',
+        resolve: {
+          resolvedFeeds: resolvedFeeds({limitComments: true})
+        }
       })
       .when('/post/:postId', {
         templateUrl: '/modules/posts/views/feed.html',
         controller: 'PostsCtrl',
         resolve: {
-          resolvedFeeds: [
-            '$route',
-            'appPostsFeed',
-            function($route, appPostsFeed) {
-              var deferred = Q.defer();
-              var options = angular.extend({
-                feedPage: 0
-              }, $route.current.params);
-
-              appPostsFeed.getFeeds(options, function(response) {
-                deferred.resolve(response);
-              });
-
-              return deferred.promise;
-            }
-          ]
+          resolvedFeeds:  resolvedFeeds({limitComments: false})
         }
       })
       ;
     $locationProvider.html5Mode(true);
   }]);
+
+function resolvedFeeds(params) {
+  return [
+    '$route',
+    'appPostsFeed',
+    function($route, appPostsFeed) {
+      var deferred = Q.defer();
+      var options = angular.extend({
+        limitComments: params.limitComments || false
+      }, $route.current.params);
+
+      appPostsFeed.getFeeds(options, function(response) {
+        deferred.resolve(response);
+      });
+
+      return deferred.promise;
+    }
+  ];
+}
